443fdf0e52326e9a6c3a4b384f3bac8a46e9a48e
Thibaud Lemaire <tlemaire@enst.fr>
2017-12-05 02:30:26 +0100

    Revert "Merge remote-tracking branch 'origin/map'"
    This reverts commit ab8113d1cfef9e0a63555806fdb70bc09271c59c, reversing
    changes made to bf1fa5001f577f9dda41108b5dda505bc7da7245.

--- include/map.h
+++ include/map.h
@@ -2,26 +2,7 @@
 // Created by Thibaud Lemaire on 23/11/2017.
 //
 
-
 #ifndef OS_ROBOT_MAP_H
 #define OS_ROBOT_MAP_H
-
-#define MAP_WIDTH 24;
-#define MAP_HEIGHT 40;
-
-typedef struct {
-    char tab[MAX_WIDTH][MAX_HEIGHT];
-    int width;
-    int height;
-
-} map;
-
-
-
-void init_map(map map);
-char get_Object(map map,position_t point);
-void set_object(map map,position_t point, char objectType);
-
-
 
 #endif //OS_ROBOT_MAP_H
