18f5f546ff006b48831f37ff543d7e762e83c35b
Raphaël Avalos <raphael.avalos@telecom-paristech.fr>
2017-12-11 21:06:02 +0100

    Init rotation head

--- src/head.c
+++ src/head.c
@@ -3,9 +3,12 @@
 #include "main.h"
 #include "display.h"
 #include "head.h"
+#include "motors.h"
 
 POOL_T distance_sensor;
 POOL_T color_sensor;
+int rotate_max_left;
+int rotate_max_right;
 
 
 /**
@@ -17,12 +20,20 @@ int init_head( void )
         distance_sensor = sensor_search( LEGO_EV3_US );
         if ( !distance_sensor ) {
                 print_error( "Distance sensor not found, exit" );
-                return ( 0 );
+                return 0;
         }
         color_sensor = sensor_search( LEGO_EV3_COLOR );
         if ( color_sensor ) {
                 print_error( "Color sensor not found, exit" );
-                return ( 0 );
+                return 0;
+        }
+        if ( !tacho_is_plugged( MOTOR_ROTATE, TACHO_TYPE__NONE_ )) {
+                print_error("Head rotation motor not found, exit");
+                return 0;
+        }
+        if ( !tacho_is_plugged( MOTOR_NODE, TACHO_TYPE__NONE_ )) {
+                print_error("Head node motor not found, exit");
+                return 0;
         }
         us_set_mode_us_dc_cm( distance_sensor );
         print_console("Distance sensor found and configured");
@@ -30,7 +41,22 @@ int init_head( void )
         color_set_mode_rgb_raw( color_sensor );
         print_console("Color sensor found and configured as Raw RGB");
         color_update();
-        return ( 1 );
+
+        tacho_set_speed_sp(MOTOR_ROTATE,FIVE);
+        while(rotate_max_left != (int) tacho_get_position(MOTOR_ROTATE,0)) {
+                rotate_max_left = (int) tacho_get_position(MOTOR_ROTATE,0);
+                sleep_ms(MOTORS_PERIOD);
+        }
+        tacho_stop(MOTOR_ROTATE);
+
+        tacho_set_speed_sp(MOTOR_ROTATE,-FIVE);
+        while(rotate_max_right != (int) tacho_get_position(MOTOR_ROTATE,0)) {
+                rotate_max_right = (int) tacho_get_position(MOTOR_ROTATE,0);
+                sleep_ms(MOTORS_PERIOD);
+        }
+        tacho_stop(MOTOR_ROTATE);
+
+        return 1;
 }
 
 void *head_main(void *arg)
@@ -42,6 +68,11 @@ void *head_main(void *arg)
                 sleep_ms(DISTANCE_PERIOD);
         }
         pthread_exit(NULL);
+}
+
+void rotate_head(void)
+{
+
 }
 
 void color_update(void)
